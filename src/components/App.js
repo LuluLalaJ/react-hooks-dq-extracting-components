1a1e8600408c0cd1fc9b05ea221bbe6f50cfc533
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-14 11:07:27 -0400

    extracted components

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,6 @@ import { messages, contacts } from "../data";
 import Nav from "./Nav";
 import Main from "./Main";
 
-console.log("Messages:", messages);
-console.log("Contacts:", contacts);
-
 function App() {
   return (
     <div className="app">
